d9bec4a28353e0be7e88c31726515b76e7bc021f
yellowhatpro <yellowhatpro3119@gmail.com>
2024-04-14 13:36:53 +0530

    playing with _app page, added layout in it

---
+++ src/components/layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+import {ReactNode} from "react";
+
+export default function Layout({children} : {children: ReactNode}) {
+    return (
+        <>
+            <Navbar/>
+            {children}
+            <Footer/>
+        </>
+    )
+}
